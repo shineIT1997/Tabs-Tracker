8ff28bd539b1b70dd9a654709bdd618a77c03307
shineIT1997 <thanhdatit1997@gmail.com>
2019-05-23 14:05:45 +0700

    nav linked

--- client/src/components/home/nav.vue
+++ client/src/components/home/nav.vue
@@ -9,11 +9,11 @@
             <li><a @click="navigateTo({name: 'topRate',})">Top Rating</a></li>
             <li><a @click="navigateTo({name: 'listNew',})">New Song</a></li>
             </ul>
-            <form class="navbar-form navbar-left">
+            <form v-on:submit.prevent="searchSong" class="navbar-form navbar-left">
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search" name="search">
+                <input type="text" v-model="search" class="form-control" placeholder="Search" name="search">
             </div>
-            <button class="btn btn-default">Submit</button>
+            <button  class="btn btn-default">Submit</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
             <li><a class="btn btn-primary" role="button" @click="navigateTo({name: 'login',})">Login</a></li>
@@ -22,17 +22,42 @@
         </div>
         </nav>
     </div>
-
 </template>
 
 <script>
 
 export default {
   name: 'nav-menu',
+  data () {
+    return {
+      search: ''
+    }
+  },
+  watch: {
+    search (value) {
+      const route = this.$router // get router (tham chiếu đến search trong data)
+      if (this.search !== '') {
+        route.query = {
+          search: this.search
+        } // parse search info in params search in router
+      }
+      this.$router.push(route)
+    },
+    '$route.query.search': {
+      immediate: true,
+      handler (value) {
+        this.search = value
+      } // make value for search data (this.search) when add value at router
+    }
+  },
   methods: {
     navigateTo (route) {
       console.log(route)
       this.$router.push(route)
+    },
+    searchSong () {
+      // when submit search form , call API search
+      console.log(this.$router.query.search)
     }
   }
 }
